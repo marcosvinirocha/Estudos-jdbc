1c665aa4f0a2865ec2c6d01bb1b4b5c76bf78a73
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2021-02-09 20:01:12 -0300

    update

---
+++ src/test/java/com/algar/jdbc/AlgarJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algar.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgarJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
